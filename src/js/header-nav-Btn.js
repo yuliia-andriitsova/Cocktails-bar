fba687bcf58a01ecdccb4694f28db326b8c7cdd5
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-09-29 14:26:31 +0300

    r
    r

--- src/js/header-nav-Btn.js
+++ src/js/header-nav-Btn.js
@@ -1,9 +1,14 @@
+// import debounce from 'lodash.debounce';
+const debounce = require('lodash.debounce');
 const refsFavorite = {
   favoriteMenu: document.querySelector('.header__favorite-box'),
   favoriteBtn: document.querySelector('.header__nav-text--btn'),
 };
 
-refsFavorite.favoriteBtn.addEventListener('mousemove', onFavoriteBtnClick);
+refsFavorite.favoriteBtn.addEventListener('mouseover', onFavoriteBtnClick);
+refsFavorite.favoriteMenu.addEventListener('mouseout', onFavoriteBtnClick);
+
+refs.input.addEventListener('input', debounce(getData, DELAY));
 
 function onFavoriteBtnClick() {
   refsFavorite.favoriteMenu.classList.toggle('header__favorite-box--is-hidden');
