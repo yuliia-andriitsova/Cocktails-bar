98543596eb0e878ab1b62f823628bbaa0df0d11f
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-10-03 09:59:27 +0300

    r
    r

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ import './js/day-night-theme';
 import './js/change-page';
 import './js/add-to-favorite';
 import './js/light-scroll';
-import './js/sound';
+// import './js/sound';
--- src/js/add-to-favorite.js
+++ src/js/add-to-favorite.js
@@ -2,12 +2,22 @@ const catalogueListFavoriteCocktails = document.querySelector(
   'body > main > div.favorite-cocktails-page > section > div > ul'
 );
 
+// Створюємо масив, який забирає масив із локал сторедж або пустий
+let favoriteCocktailsEl = JSON.parse(localStorage.savedCocktails || '[]');
+
 setTimeout(() => {
+  const asdasd = document.querySelector(
+    'body > main > div.main-page > section.catalogue.container > div > ul > li:nth-child(1) > div > div > button.btn__transparent'
+  );
+  console.dir(asdasd);
+  // Отримуємо доступ до кнопки
+  // const favoritesCocktailsBtnRemove =
+  //   document.querySelectorAll('.btn__transparent');
+  // console.log(favoritesCocktailsBtnRemove);
+
   const refsFav = {
     addToFav: document.querySelectorAll('.catalogue__item'),
   };
-
-  let favoriteCocktailsEl = [];
 
   const allCocktailsEl = refsFav.addToFav;
 
@@ -18,32 +28,35 @@ setTimeout(() => {
       BtnToFavoriteCocktails =
         allCocktailsEl[i].children[1].children[1].children[1];
 
+      // BtnToFavoriteCocktails.firstChild.data = 'Remove';
+      // console.dir(BtnToFavoriteCocktails.firstChild.data);
+
       BtnToFavoriteCocktails.addEventListener(
         'click',
         onClickBtnToAddFavoriteCocktails
       );
 
-      let parsedCocktailsData = ';';
-
       function onClickBtnToAddFavoriteCocktails() {
-        // console.log(allCocktailsEl[i]);
+        // BtnToFavoriteCocktails.firstChild.data = 'Remove';
+        // console.log(BtnToFavoriteCocktails.firstChild.data);
 
+        // Перетворюємо html код в строку
         let htmlCoctailCard = new XMLSerializer().serializeToString(
           allCocktailsEl[i]
         );
-        favoriteCocktailsEl.push(htmlCoctailCard);
-
+        // BtnToFavoriteCocktails.firstChild.data = 'Remove';
+        // Перевірка, якщо наш масив не містить карту то пушим карту + рендерим
+        if (!favoriteCocktailsEl.includes(htmlCoctailCard)) {
+          favoriteCocktailsEl.push(htmlCoctailCard);
+          catalogueListFavoriteCocktails.insertAdjacentHTML(
+            'beforeend',
+            htmlCoctailCard
+          );
+        }
+        // Створюємо локальне сховище, передаємо туда масив
         localStorage.setItem(
-          'FAVORITE_COCKTAILS',
+          'savedCocktails',
           JSON.stringify(favoriteCocktailsEl)
-        );
-        let savedCocktailsData = localStorage.getItem('FAVORITE_COCKTAILS');
-        parsedCocktailsData = JSON.parse(savedCocktailsData).join('');
-        console.log(parsedCocktailsData);
-
-        catalogueListFavoriteCocktails.insertAdjacentHTML(
-          'beforeend',
-          parsedCocktailsData
         );
       }
     }
@@ -51,25 +64,12 @@ setTimeout(() => {
   allCocktailsMap();
 }, 4000);
 
-// function addFavoriteCocktailToLocalStorage(e) {
-//   const currentEl = e;
-//   const maket = `<li class="catalogue__item">
-//     <img src="https://www.thecocktaildb.com/images/media/drink/mql55h1643820632.jpg" alt="" min-width="280" loading="lazy">
-//     <div class="catalogue__description">
-//       <p class="catalogue__text">A.D.M. (After Dinner Mint)</p>
-//       <div class="catalogue__btn">
-//         <button class="btn__orange" type="button">Learn more</button>
-//         <button class="btn__transparent" type="button">
-//           Add to
+let savedCocktailsDataLS = localStorage.getItem('savedCocktails');
+let parsedCocktailsDataLS = JSON.parse(savedCocktailsDataLS).join('');
+// console.log(parsedCocktailsDataLS);
+// console.log(localStorage.savedCocktails);
 
-//           <svg class="svg__btn" width="21" height="19" viewBox="0 0 21 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-//             <path d="M9.64552 16.8891L9.64408 16.8878C6.91999 14.4519 4.74082 12.5008 3.23063 10.68C1.73147 8.87263 1 7.31869 1 5.69482C1 3.07114 3.08006 1 5.775 1C7.3044 1 8.784 1.70692 9.74314 2.81762L10.5 3.69406L11.2569 2.81762C12.216 1.70692 13.6956 1 15.225 1C17.9199 1 20 3.07114 20 5.69482C20 7.31871 19.2685 8.87269 17.7692 10.6816C16.2589 12.5036 14.0801 14.457 11.3563 16.8978C11.3559 16.8982 11.3555 16.8985 11.3551 16.8989L10.5025 17.6584L9.64552 16.8891Z" stroke="#FD5103" stroke-width="2"></path>
-//           </svg>
-//         </button>
-//       </div>
-//     </div>
-//   </li>`;
-//   console.log(currentEl);
-//   console.log(maket);
-//   localStorage.setItem('FAVORITE_COCKTAILS', JSON.stringify(maket));
-// }
+catalogueListFavoriteCocktails.insertAdjacentHTML(
+  'beforeend',
+  parsedCocktailsDataLS
+);
