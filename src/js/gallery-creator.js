8d0cea7889af55b9d3bd75d6ce2113858d455b30
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-10-03 21:21:46 +0300

    r
    r

--- src/js/gallery-creator.js
+++ src/js/gallery-creator.js
@@ -3,7 +3,7 @@ import noFindAnyCoctail from '../template/not-found-cocktails.hbs';
 import { getApiData } from './rendering-catalogue';
 import { checkingScreenWidth } from './cheking-screen-width';
 import modalCoctails from '../template/modal-cocktails.hbs';
-import modalIngredients from '../template/modal-ingredients.hbs';
+
 // const InfiniteScroll = require('infinite-scroll');
 
 const refsGallery = {
@@ -63,7 +63,7 @@ function ifNoFindAnyCocktail(r) {
     let arr = [];
     for (let i = 0; i < checkingScreenWidth; i += 1) {
       if (r[i]) {
-        arr.push(refactoringCocktailsArray(r)[i]);
+        arr.push(r[i]);
       }
     }
     refsGallery.catalogueTitle.textContent = 'Cocktails';
@@ -75,21 +75,6 @@ function ifNoFindAnyCocktail(r) {
       noFindAnyCoctail()
     );
   }
-}
-
-function refactoringCocktailsArray(elements) {
-  return elements.map(el => {
-    let arr = [];
-
-    for (let key of Object.keys(el)) {
-      for (let i = 1; i < 15; i += 1) {
-        if (key === `strIngredient${i}` && el[key] !== null) {
-          arr.push(el[key]);
-          el.strIngredient = arr;
-        }
-      }
-    }
-  });
 }
 
 function resetContent() {
@@ -104,20 +89,13 @@ const refsModal = {
 };
 
 let markup = modalCoctails();
-let markupIngredients = modalIngredients();
+
 function CreateModal(e) {
   if (e.target.classList.contains('open-modal-button')) {
     toggleModals();
     refsModal.modal.insertAdjacentHTML('beforeend', markup);
     const closeModalBtn = document.querySelector('[data-modal-close]');
     closeModalBtn.addEventListener('click', toggleModals);
-    // ---Іванка-----
-    const openModaIngred = document.querySelector('.modal-first__list');
-    console.log(openModaIngred);
-    if (!refsModal.modal.classList.contains('is-hidden')) {
-      openModaIngred.addEventListener('click', openModalIng);
-    }
-    // ----Іванка---
     markup = '';
   }
 }
@@ -127,6 +105,7 @@ function toggleModals() {
   refsModal.modal.classList.toggle('is-hidden');
 }
 // ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑Sergey↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+<<<<<<< HEAD
 
 //  ----------Іванка---------------
 let nameIngredient = '';
@@ -158,3 +137,5 @@ function closeOnClick(event) {
   }
 }
 // -----Іванка----
+=======
+>>>>>>> parent of 8f07eea (Merge branch 'main' of https://github.com/Vokoloven/hardcore-bar into header/oles)
